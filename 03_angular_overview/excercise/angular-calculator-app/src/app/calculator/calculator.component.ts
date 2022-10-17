5d191371035a6aeb178052eb2b92c38544b57b4f
Hà Bảo Ân <you@example.combaoandlk93@gmail.com>
2022-10-17 13:28:33 +0700

    ok

---
+++ 03_angular_overview/excercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  numberFirst: number | undefined;
+  numberSecond: number | undefined;
+  resultNum: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  sumNumber(): void {
+    this.resultNum = Number(this.numberFirst) + Number(this.numberSecond);
+  }
+
+  minusNumber(): void {
+    this.resultNum = Number(this.numberFirst) - Number(this.numberSecond);
+  }
+
+
+  multiplication(): void {
+    this.resultNum = Number(this.numberFirst) * Number(this.numberSecond);
+  }
+
+  division(): void {
+    if (Number(this.numberSecond) != 0) {
+       this.resultNum = Number(this.numberFirst) / Number(this.numberSecond);
+    }else {
+      this.resultNum = "Không thể chia cho 0";
+    }
+  }
+}
