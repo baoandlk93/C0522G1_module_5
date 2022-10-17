52cef39877eed06d3f50ac0c3ad61aab5b330a26
Hà Bảo Ân <you@example.combaoandlk93@gmail.com>
2022-10-17 16:02:53 +0700

    17/10/2022

---
+++ angular-case-study/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FacilityComponent } from './facility/facility.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    EmployeeComponent,
+    FacilityComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
